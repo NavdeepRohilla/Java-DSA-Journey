c710729f133d93aff0ec3d272e1af10a05b16258
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-03-11 00:37:52 +0530

    Learn Some Basics of Java Output and Comments in Java

---
+++ Day-2/JavaBasics.java
@@ -0,0 +1,40 @@
+public  class JavaBasics {
+
+    public static void main(String[] args) {
+    // Output in Java
+        /*There are three methods for the output in java
+        1. print
+        2. println
+        3. "\n"
+
+    */
+        // By print
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+        System.out.print("Hello World");
+
+        // By println
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        // By "\n"
+        System.out.print("Hello World\nHello World\nHello World\n");
+
+
+        // Print Pattern
+        // ****
+        // ***
+        // **
+        // *
+
+        System.out.println("Print Pattern \n****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
+// Comments in Java :- 
+// Single line comment is written by //
+/* Multiline comment is written between these
+/* */
