6156aff7b917366bceb44ad28c939e11d9470537
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-03-11 00:41:34 +0530

    Learned about datatypes in Java

---
+++ Day-2/dataTypes.java
@@ -0,0 +1,36 @@
+/*
+Data Types :- In python DataType represent the kind of value which determines what operations can be performed on the data. 
+There are two types of Data TYpes in Python :-
+
+1. Primitive Data Types : byte  short , char , boolean , int , long , float , double
+2. Non Primitive Data Types : string , Array , Class , Object , Interface
+*/
+
+public class dataTypes {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean var = true;
+        System.out.println(var);
+
+        float price = 10.5f;
+        System.out.println(price);
+
+        int number = 25;
+        System.out.println(number);
+
+        long bigNumber = 8415854512l;
+        System.out.println(bigNumber);
+
+        double decimalNumber = 458.25;
+        System.out.println(decimalNumber);
+
+        short n = 240;
+        System.out.println(n);
+
+    }
+}
