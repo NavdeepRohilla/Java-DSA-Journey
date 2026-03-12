64f16784f5fd9a78e74ebd5a1bcee755413ebd15
Navdeep Rohilla <navdeeprohilla03@gmail.com>
2026-03-12 14:42:27 +0530

    Learned about Operators and type of Operators

---
+++ Day-3/Operators.java
@@ -0,0 +1,25 @@
+/*
+Operators in Java:-
+
+- Symbols that tell compiler to perform some operations.
+
+Sum = a + b;
+here,
+a & b are operands and '+' is an operator.
+
+Types Of Operators :-
+1. Arithmetic Operators
+2. Relational Operators
+3. Logical Operators
+4. Bitwise Operators
+5. Assignment Operators
+*/
+public class Operators {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 18;
+
+        int sum = a + b;
+        System.out.println("Sum : " + sum);
+    }
+}
